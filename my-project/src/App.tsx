f70c4b43c41fb9f5c04695a1465e1664eedd1c75
suniaikin <class.suniaikin@gmail.com>
2025-11-07 13:03:57 -0800

    add

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -1,10 +1,35 @@
+const tracks = [
+    {
+        title: "Musicfun soundtrack",
+        url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
+    },
+    {
+        title: "Musicfun soundtrack instrumental",
+        url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
+    },
+];
+
 function App() {
 
-  return (
-    <>
+	const liReactElements = [
+        <li>
+            <div>{tracks[0].title}</div>
+            <audio controls src={tracks[0].url}></audio>
+        </li>,
+        <li>
+            <div>{tracks[1].title}</div>
+            <audio controls src={tracks[1].url}></audio>
+        </li>,
+    ];
 
-    </>
-  )
+    return (
+        <>
+            <h1>Musicfun player</h1>
+            <ul>
+				{liReactElements}
+            </ul>
+        </>
+    );
 }
 
-export default App
+export default App;
